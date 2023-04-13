d6becb4a88df1adde0b50520855a24205cb2e5d9
juliakocur <juliakocur@mail.com>
2023-04-13 22:42:19 +0200

    docs: add solutions

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,43 +6,127 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.right = null;
+    this.left = null;
+    }
+    add(data) {//2.если root есть то код идет сюда
+    if(data < this.data) {
+    this.addLeft(data)
+    } else {
+    this.addRight(data)
+    }
+    }
+    addLeft(data) {
+    if(this.left) {
+    return this.left.add(data);//чтобы шло по кругу если лево занято
+    }
+    this.left = new Node(data);
+    }
+    addRight(data) {
+    if(this.right) {
+    return this.right.add(data);//чтобы шло по кругу если право занято
+    }
+    this.right = new Node(data);
+    }
+ }   
 class BinarySearchTree {
+   constructor() {
+   this.root = null;
+   }
+  
+   root() {
+   return this.root
+   }
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   add(data) {//1.
+   if(this.data === data) return;//если пришел повторяющийся элемент то ничего не делать
+   if (this.root) { 
+    return this.root.add(data);
+  } 
+   this.root = new Node(data);
   }
-
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  find(data) {
+  let current = this.root;// если в root нет элемента то он говорит undefind и дальше идет в while
+  
+  while(true) {
+  if(current === undefined) 
+  return console.log('no element'); // если не найдено то будет undefind 
+  if(current.data === data) { //если нашли то возвращаем узел
+  console.log('found')
+  return current;
   }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  if(data < current.data) {
+  current = current.left;//идти влево искать
+  }else{
+  current = current.right;//идти вправо искать
+  } 
   }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  has(data) {
+    let current = this.root;// если в root нет элемента то он говорит undefind и дальше идет в while
+  
+  while(true) {
+  if(current === undefined) {
+  return false; 
+  console.log('no element');
+  } // если не найдено то будет undefind 
+  if(current.data === data) { //если нашли то возвращаем узел
+  console.log('found')
+  return true;
   }
-
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  if(data < current.data) {
+  current = current.left;//идти влево искать
+  }else{
+  current = current.right;//идти вправо искать
+  } 
   }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 }
+
+//class BinarySearchTree {
+//
+//  root() {
+//    throw new NotImplementedError('Not implemented');
+//    // remove line with error and write your code here
+//  }
+//
+//  add(/* data */) {
+//    throw new NotImplementedError('Not implemented');
+//    // remove line with error and write your code here
+//  }
+//
+//  has(/* data */) {
+//    throw new NotImplementedError('Not implemented');
+//    // remove line with error and write your code here
+//  }
+//
+//  find(/* data */) {
+//    throw new NotImplementedError('Not implemented');
+//    // remove line with error and write your code here
+//  }
+//
+//  remove(/* data */) {
+//    throw new NotImplementedError('Not implemented');
+//    // remove line with error and write your code here
+//  }
+//
+//  min() {
+//    throw new NotImplementedError('Not implemented');
+//    // remove line with error and write your code here
+//  }
+//
+//  max() {
+//    throw new NotImplementedError('Not implemented');
+//    // remove line with error and write your code here
+//  }
+//}
 
 module.exports = {
   BinarySearchTree
