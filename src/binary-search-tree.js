c6abcbfdd8698d48131fa2f8a3e3501d01ee6b64
juliakocur <juliakocur@mail.com>
2023-04-14 18:08:55 +0200

    docs: add solution for binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,61 +10,60 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 class Node {
   constructor(data) {
-    this.data = data;
-    this.right = null;
-    this.left = null;
+      this.data = data;
+      this.right = null;
+      this.left = null;
     }
-    add(data) {//2.если root есть то код идет сюда
-    if(data < this.data) {
-    this.addLeft(data)
+      add(data) {//2.если root есть то код идет сюда
+      if(data < this.data) {
+      this.addLeft(data)
     } else {
-    this.addRight(data)
+      this.addRight(data)
     }
     }
     addLeft(data) {
-    if(this.left) {
-    return this.left.add(data);//чтобы шло по кругу если лево занято
-    }
-    this.left = new Node(data);
+      if(this.left) {
+      return this.left.add(data);//чтобы шло по кругу если лево занято
+      }
+      this.left = new Node(data);
     }
     addRight(data) {
-    if(this.right) {
-    return this.right.add(data);//чтобы шло по кругу если право занято
+      if(this.right) {
+      return this.right.add(data);//чтобы шло по кругу если право занято
     }
-    this.right = new Node(data);
+      this.right = new Node(data);
     }
  }   
 class BinarySearchTree {
    constructor() {
-   this.rootNum = null;
+    this.rootNum = null;
    }
   
    root() {
-   return this.rootNum
+    return this.rootNum
    }
 
    add(data) {//1.
-   if(this.data === data) return;//если пришел повторяющийся элемент то ничего не делать
-   if (this.rootNum) { 
-    return this.rootNum.add(data);
+    if(this.data === data) return;//если пришел повторяющийся элемент то ничего не делать
+    if (this.rootNum) { 
+  return this.rootNum.add(data);
   } 
    this.rootNum = new Node(data);
   }
   
   find(data) {
-  let current = this.rootNum;// если в root нет элемента то он говорит undefind и дальше идет в while
+    let current = this.rootNum;// если в root нет элемента то он говорит undefind и дальше идет в while
   
   while(true) {
-  if(current === null||current === undefined) 
+    if(current === null||current === undefined) 
   return null; console.log('no element'); // если не найдено то будет undefind 
-  if(current.data === data) { //если нашли то возвращаем узел
-  console.log('found')
+    if(current.data === data) { //если нашли то возвращаем узел
   return current;
   }
   if(data < current.data) {
-  current = current.left;//идти влево искать
+    current = current.left;//идти влево искать
   }else{
-  current = current.right;//идти вправо искать
+    current = current.right;//идти вправо искать
   } 
   }
   }
@@ -73,107 +72,86 @@ class BinarySearchTree {
     let current = this.rootNum;// если в root нет элемента то он говорит undefind и дальше идет в while
   
   while(true) {
-  if(current === null||current === undefined||data === null||data === undefined) {
+    if(current === null||current === undefined||data === null||data === undefined) {
   return false; 
-  console.log('no element');
   } // если не найдено то будет undefind 
-  if(current.data === data) { //если нашли то возвращаем узел
-  console.log('found')
+    if(current.data === data) { //если нашли то возвращаем узел
   return true;
   }
   if(data < current.data) {
-  current = current.left;//идти влево искать
+    current = current.left;//идти влево искать
   }else{
-  current = current.right;//идти вправо искать
+    current = current.right;//идти вправо искать
   } 
   }
   }
 
   minNode(node) {
     // если слева от узла ноль тогда это должен быть минимальный узел
-  if (node.left === null)
-  return node;
-  else
-  return this.minNode(node.left);
+  if (node.left === null) {
+    return node;
+  } else {
+    return this.minNode(node.left);
+  }
   }
 
   remove(data) {
-  this.rootNum = this.removeNode(this.rootNum, data); // helper method below
+    this.rootNum = this.removeNode(this.rootNum, data); // helper method below
   }
   removeNode(node, data) {
-  if (node === null) {
+    if (node === null) {
   return null;
     // если данные, которые нужно удалить, меньше, чем данные корня, переходим к левому поддереву
   } else if (data < node.data) {
-  node.left = this.removeNode(node.left, data);
+    node.left = this.removeNode(node.left, data);
   return node;
     // если данные, которые нужно удалить, больше, чем данные корня, переходим к правому поддереву
   } else if (data > node.data) {
-  node.right = this.removeNode(node.right, data);
+    node.right = this.removeNode(node.right, data);
   return node;
     // если данные такие как данные корня, удаляем узел
   } else {
         // удаляем узел без потомков (листовой узел (leaf) или крайний)
   if (node.left === null && node.right === null) {
-  node = null;
+    node = null;
   return node;
   }
         // удаляем узел с одним потомком
   if (node.left === null) {
-  node = node.right;
+    node = node.right;
   return node;
   } else if(node.right === null) {
-  node = node.left;
+    node = node.left;
   return node;
   }
         // удаляем узел с двумя потомками
         // minNode правого поддерева хранится в новом узле
   let newNode = this.minNode(node.right);
-  node.data = newNode.data;
-  node.right = this.removeNode(node.right, newNode.data);
+    node.data = newNode.data;
+    node.right = this.removeNode(node.right, newNode.data);
   return node;
   }
 }
 
+min() {
+ if (this.rootNum === null) return null;
+  // если слева от узла ноль тогда это должен быть минимальный узел
+ let node = this.rootNum;
+  while (node.left) {
+    node = node.left;
+  }
+  return node.data;// передать значение этой ноды
 }
 
-//class BinarySearchTree {
-//
-//  root() {
-//    throw new NotImplementedError('Not implemented');
-//    // remove line with error and write your code here
-//  }
-//
-//  add(/* data */) {
-//    throw new NotImplementedError('Not implemented');
-//    // remove line with error and write your code here
-//  }
-//
-//  has(/* data */) {
-//    throw new NotImplementedError('Not implemented');
-//    // remove line with error and write your code here
-//  }
-//
-//  find(/* data */) {
-//    throw new NotImplementedError('Not implemented');
-//    // remove line with error and write your code here
-//  }
-//
-//  remove(/* data */) {
-//    throw new NotImplementedError('Not implemented');
-//    // remove line with error and write your code here
-//  }
-//
-//  min() {
-//    throw new NotImplementedError('Not implemented');
-//    // remove line with error and write your code here
-//  }
-//
-//  max() {
-//    throw new NotImplementedError('Not implemented');
-//    // remove line with error and write your code here
-//  }
-//}
+max() {
+  if (this.rootNum === null) return null;
+  let node = this.rootNum;
+  while (node.right) {
+    node = node.right;
+  }
+  return node.data;
+}
+}
 
 module.exports = {
   BinarySearchTree
